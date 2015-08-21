d9e0558b2a204c39bebce66f0aafcba05919feb6
Jonathan Carroll <jonathaniscarroll@gmail.com>
2015-08-20 23:27:36 -0400

    adding functionality to try to make gift cards automatically load the correct vista print pages

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 var item = 1;
 var itemUrls = [
-  'http://www.vistaprint.ca/vp/gateway.aspx?s=4009498034&preurl=%2fshare-design.aspx%3fdoc_id%3d3051779549%26shopper_id%3dWEQ5W14PTOE164EQT460QV4RS1LEJLHW%26xnav%3dsharesource_8%26share_key%3d96e14e35-6786-4e10-8e7a-98013e877c98',
-  'http://www.vistaprint.ca/vp/gateway.aspx?s=4009498034&preurl=%2fshare-design.aspx%3fdoc_id%3d3051774391%26shopper_id%3dWEQ5W14PTOE164EQT460QV4RS1LEJLHW%26xnav%3dsharesource_8%26share_key%3dd09e30b6-5de8-46be-801f-fb27e11e6b78',
-  'http://www.vistaprint.ca/vp/gateway.aspx?s=4009498034&preurl=%2fshare-design.aspx%3fdoc_id%3d3051779354%26shopper_id%3dWEQ5W14PTOE164EQT460QV4RS1LEJLHW%26xnav%3dsharesource_8%26share_key%3d141d69b4-6ef7-4442-9615-5f2236ff7f60',
-  'http://www.vistaprint.ca/vp/gateway.aspx?s=4009498034&preurl=%2fshare-design.aspx%3fdoc_id%3d3093587310%26shopper_id%3dWEQ5W14PTOE164EQT460QV4RS1LEJLHW%26xnav%3dsharesource_8%26share_key%3d8bc761cd-830b-468e-9ee2-d745e6af0f6f',
-  ' http://www.vistaprint.ca/vp/gateway.aspx?s=4009498034&preurl=%2fshare-design.aspx%3fdoc_id%3d3093590337%26shopper_id%3dWEQ5W14PTOE164EQT460QV4RS1LEJLHW%26xnav%3dsharesource_8%26share_key%3d23df1063-1caa-4711-94b8-2cce82b4b3cb'
+  'tshirt.html',
+  'tshirt.html',
+  'tshirt.html',
+  'tshirt.html',
+  'tshirt.html'
 ];
 
 $(document).ready(function(){
@@ -23,8 +23,9 @@ $(document).ready(function(){
       
       item++;
       console.log(item);
+      console.log(itemUrls[item]);
       
-      //$('#purchaseFrame').attr('src', itemUrls[item]);
+      $('.modal-body').load(itemUrls[item]);
       
       $('#purchaseModal').modal('show');
     });  
@@ -39,7 +40,7 @@ $(document).ready(function(){
      
     if ( $(this).is(':last-child') ) {
      $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
-      alert('Na-na!');
+      alert('Na-na!'); 
      } else {
         $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
     } 
